87166c0913078d792a482134acf748d23439569b
Kakarot-SSJ4 <akashspi09@gmail.com>
2020-04-25 18:16:59 +0530

    created notebook for merging

--- src/mergeHabitatAndHealth.py
+++ src/mergeHabitatAndHealth.py
@@ -4,7 +4,7 @@ with open('../data/healthCleanReduced.csv', 'r') as healthCareReducedCSV:
 	healthCareReducedList = list(healthCareReducedCSV)
 	with open('../data/habitation_reduced.csv', 'r') as habitationReduced:
 		habitationReducedList = list(habitationReduced)
-		stateSetInHabitation = dict()
+		districtSetInHabitation = dict()
 		headingRow = True
 		indexInHabitation = 1
 		for rowHabitation in habitationReducedList:
@@ -12,25 +12,25 @@ with open('../data/healthCleanReduced.csv', 'r') as healthCareReducedCSV:
 				headingRow = False
 				continue
 			rowList = rowHabitation.split(",")
-			stateSetInHabitation[rowList[1].upper().strip()] = indexInHabitation
+			districtSetInHabitation[rowList[1].upper().strip()] = indexInHabitation
 			indexInHabitation += 1
 
-		stateSetInHealth = dict([])
+		districtSetInHealth = dict([])
 		headingRow = True
 		for rowHealth in healthCareReducedList:
 			if headingRow:
 				headingRow = False
 				continue
 			rowList = rowHealth.split(",")
-			stateSetInHealth[rowList[1].upper().strip()] = rowList[2] + "," + rowList[3] + "," + rowList[4] + "," + rowList[5] + "," + rowList[6]
+			districtSetInHealth[rowList[1].upper().strip()] = rowList[2] + "," + rowList[3] + "," + rowList[4] + "," + rowList[5] + "," + rowList[6]
 
 		reducedCSVList = []
 		reducedCSVList.append(['State', 'District', 'SC Current', 'ST Current', 'General Current', 'SC Covered', 'ST Covered', 'General Covered', 'Number of Sub Centres', 'Number of Primary Health Centres', 'Number of Community Health Centres', 'Sub Divisional Hospitals', 'District Hospitals'])
 
-		for stateHabitat in stateSetInHabitation:
-			if(stateHabitat in stateSetInHealth):
-				tempList = habitationReducedList[stateSetInHabitation[stateHabitat]].split(",")
-				tempValue = stateSetInHealth[stateHabitat].strip().split(",")
+		for districtHabitat in districtSetInHabitation:
+			if(districtHabitat in districtSetInHealth):
+				tempList = habitationReducedList[districtSetInHabitation[districtHabitat]].split(",")
+				tempValue = districtSetInHealth[districtHabitat].strip().split(",")
 				reducedCSVList.append([tempList[0], tempList[1], tempList[2], tempList[3], tempList[4], tempList[5], tempList[6], tempList[7].strip(), tempValue[0], tempValue[1], tempValue[2], tempValue[3], tempValue[4]])
 
 
